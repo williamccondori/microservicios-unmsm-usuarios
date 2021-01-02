eb3f84f25b2c7e74114d64c000f7e27c7846ddaa
William Condori <williamccondori@gmail.com>
2021-01-02 11:48:34 -0500

    Se agrega cliente eureka

--- src/main/java/com/concepto/unmsm/microservicios/app/usuarios/MicroserviciosUnmsmUsuariosApplication.java
+++ src/main/java/com/concepto/unmsm/microservicios/app/usuarios/MicroserviciosUnmsmUsuariosApplication.java
@@ -2,8 +2,10 @@ package com.concepto.unmsm.microservicios.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviciosUnmsmUsuariosApplication {
 
     public static void main(String[] args) {
